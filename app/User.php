3f2f85f19e3f091dfa1c8f5521372d1803981d7b
Alexandre Leva <alexandre.leva@materiauxnet.com>
2022-08-25 12:18:16 +0100

    comment softDelete to totally delete user from the DB

--- app/User.php
+++ app/User.php
@@ -13,7 +13,8 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use SoftDeletes, Notifiable, HasApiTokens;
+    use //SoftDeletes, 
+    Notifiable, HasApiTokens;
 
     public $table = 'users';
 
